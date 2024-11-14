e63a004b6570f2518266c3e2b9e83aee16f9ff51
Nicolae <nicumihailovtic@gmail.com>
2024-11-14 21:49:41 +0200

    Laborator4_finalizat

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Mihailov_Nicolae_Laborator2.Data;
 using Mihailov_Nicolae_Laborator2.Models;
+using Mihailov_Nicolae_Laborator2.Models.ViewModels;
 
 namespace Mihailov_Nicolae_Laborator2.Pages.Categories
 {
@@ -21,9 +22,31 @@ namespace Mihailov_Nicolae_Laborator2.Pages.Categories
 
         public IList<Category> Category { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Category = await _context.Category.ToListAsync();
+            CategoryData = new CategoryIndexData();
+            var categoriesQuery = _context.Category
+                .Include(c => c.BookCategories)
+                .ThenInclude(bc => bc.Book)
+                .ThenInclude(b => b.Author)
+                .OrderBy(c => c.CategoryName);
+
+            CategoryData.Categories = await categoriesQuery.ToListAsync();
+
+            if (id.HasValue)
+            {
+                CategoryID = id.Value;
+
+                var selectedCategory = CategoryData.Categories
+                    .FirstOrDefault(c => c.ID == id.Value);
+
+                CategoryData.Books = selectedCategory?.BookCategories
+                    .Select(bc => bc.Book)
+                    .ToList();
+            }
         }
     }
 }
