99fa404c2bb669a1f2150e93913f22bb77de3b94
Nicolae <nicumihailovtic@gmail.com>
2024-11-18 02:22:03 +0200

    Laborator_6

--- Program.cs
+++ Program.cs
@@ -5,8 +5,23 @@ using Microsoft.AspNetCore.Identity;
 using Mihailov_Nicolae_Laborator2.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy =>
+   policy.RequireRole("Admin"));
+});
+
+
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Books");
+    options.Conventions.AllowAnonymousToPage("/Books/Index");
+    options.Conventions.AllowAnonymousToPage("/Books/Details");
+    options.Conventions.AuthorizeFolder("/Members", "AdminPolicy");
+    options.Conventions.AuthorizeFolder("/Publishers", "AdminPolicy");
+    options.Conventions.AuthorizeFolder("/Categories", "AdminPolicy");
+});
 builder.Services.AddDbContext<Mihailov_Nicolae_Laborator2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Mihailov_Nicolae_Laborator2Context") ?? throw new InvalidOperationException("Connection string 'Mihailov_Nicolae_Laborator2Context' not found.")));
 
@@ -16,6 +31,7 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("Mihailov_Nicolae
     throw new InvalidOperationException("Connection string 'Mihailov_Nicolae_Laborator2Context' not found.")));
 builder.Services.AddDefaultIdentity<Mihailov_Nicolae_Laborator2User>(options =>
 options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
